e1e1bd1cbf4148a38825ef88d592c7c07cd5ac9c
Katie McLaughlin <glasnt@google.com>
2019-09-10 10:59:53 +1000

    Add missing licences
    Change-Id: I39565f242439d0aa3974c6bd54c038a089830521

--- .cloudbuild/berglas_setup.sh
+++ .cloudbuild/berglas_setup.sh
@@ -1,3 +1,19 @@
+#!/bin/bash -eu
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 export DATABASE_URL=berglas://${PROJECT_ID}-secrets/database_url?destination=/secrets/database_url
 export SECRET_KEY=berglas://${PROJECT_ID}-secrets/secret_key?destination=/secrets/secret_key
 export BUCKET_NAME=berglas://${PROJECT_ID}-secrets/bucket_name?destination=/secrets/bucket_name
