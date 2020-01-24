a4f7f16d55520648b9e69d5aea10caee382e42e4
Katie McLaughlin <katie@glasnt.com>
2020-01-24 14:45:40 +1100

    variables should be defined: type, description[, default]

--- terraform/database/variables.tf
+++ terraform/database/variables.tf
@@ -1,20 +1,21 @@
 variable "project" {
   type        = string
-  description = "The Google Cloud Platform project name"
+  description = "Google Cloud Platform project name"
 }
 
 variable "region" {
-  default = "us-central1"
   type    = string
+  description  = "Google Cloud Platform region name"
+  default = "us-central1"
 }
 
 variable "instance_name" {
-  description = "Name of the postgres instance to use (for your project and region)"
   type        = string
+  description = "Name of the postgres instance to use (for your project and region)"
 }
 
 variable "slug" {
-  description = "A prefix slug for this instance (e.g prod, test)"
   type        = string
+  description = "A prefix slug for this instance (e.g prod, test)"
 }
 
--- terraform/permissions/variables.tf
+++ terraform/permissions/variables.tf
@@ -1,19 +1,19 @@
 variable "project" {
   type        = string
-  description = "The Google Cloud Platform project name"
+  description = "Google Cloud Platform project name"
 }
 
 variable "berglas_bucket" {
-  description = "The bucket **that has already been boostrapped** by berglas"
   type        = string
+  description = "The bucket **that has already been boostrapped** by berglas"
 }
 
 variable "slug" {
-  description = "A prefix slug for this instance (e.g prod, test)"
   type        = string
+  description = "A prefix slug for this instance (e.g prod, test)"
 }
 
 variable "database_url" {
-  description = "The ODBC connection string"
   type        = string
+  description = "The ODBC connection string"
 }
