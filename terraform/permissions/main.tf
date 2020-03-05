0eaa5d1c34895394f2bb27e68019b8cb2d56c69b
Katie McLaughlin <katie@glasnt.com>
2020-03-05 15:47:33 +1100

    Correctly set service account permissions
    It's not the Cloud Build service account having actAs permissions
    It's the Cloud Build service account having actAs permission on the Cloud Run
    custom service account.

--- terraform/permissions/main.tf
+++ terraform/permissions/main.tf
@@ -51,11 +51,12 @@ resource "google_project_iam_binding" "service_permissions" {
   depends_on = [google_service_account.cloudrun]
 }
 
-resource "google_project_iam_binding" "cloudbuild_sa" {
-  role    = "roles/iam.serviceAccountUser"
+resource "google_service_account_iam_binding" "cloudbuild_sa" {
+  service_account_id = google_service_account.cloudrun.name
+  role               = "roles/iam.serviceAccountUser"
+
   members = [local.cloudbuild_sa]
 }
-
 
 ###################################################################################
 # Secrets
