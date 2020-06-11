91fb837fb46cd2a637464140625c38bed9283cb6
Katie McLaughlin <katie@glasnt.com>
2020-06-12 09:50:09 +1000

    Add post-create login password access information

--- .gcloud/postcreate.sh
+++ .gcloud/postcreate.sh
@@ -18,3 +18,15 @@ gcloud run services update $K_SERVICE --platform managed --region ${GOOGLE_CLOUD
     --service-account ${K_SERVICE}@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
 
 echo "Post-create configuration complete ✨"
+
+echo ""
+echo ""
+echo "Unicodex is now deployed to ${SERVICE_URL}"
+echo ""
+echo "To login into the Django admin: "
+echo "  * go to ${SERVICE_URL}/admin"
+echo "  * login with the username and password that are stored in Secret Manager"
+echo ""
+echo "gcloud secrets versions access latest --secret SUPERUSER"
+echo "gcloud secrets versions access latest --secret SUPERPASS"
+echo ""
