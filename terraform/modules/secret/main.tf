b09b6d1291ad954df9f006df6d6328ac6cbabee1
Katie McLaughlin <katie@glasnt.com>
2020-09-22 16:35:29 +1000

    Use GA version of provider secrets

--- terraform/modules/secret/main.tf
+++ terraform/modules/secret/main.tf
@@ -1,5 +1,4 @@
 resource google_secret_manager_secret secret {
-  provider = google-beta
   project  = var.project
 
   secret_id = var.name
@@ -9,15 +8,11 @@ resource google_secret_manager_secret secret {
 }
 
 resource google_secret_manager_secret_version secret {
-  provider = google-beta
-
   secret      = google_secret_manager_secret.secret.id
   secret_data = var.secret_data
 }
 
 resource google_secret_manager_secret_iam_binding secret {
-  provider = google-beta
-
   secret_id = google_secret_manager_secret.secret.id
   role      = "roles/secretmanager.secretAccessor"
   members   = var.accessors
