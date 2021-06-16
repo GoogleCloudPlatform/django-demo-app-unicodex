23090f4bf14c2a6da70e6903478cf2591cdad470
Katie McLaughlin <katie@glasnt.com>
2021-06-16 13:03:04 +1000

    update

--- experimental/run_test.sh
+++ experimental/run_test.sh
@@ -15,24 +15,36 @@ fi
 
 echo "⏩ Quick start a $TEST_TYPE build"
 
-stepdo "🔨 create CI project"
+SA_NAME=ci-serviceaccount
+SA_EMAIL=$(gcloud iam service-accounts list --filter $SA_NAME --format 'value(email)')
+
 RANDOM_IDENTIFIER=$((RANDOM % 999999))
 export CI_PROJECT=$(printf "%s-%06d" $CI_PROJECT_PREFIX $RANDOM_IDENTIFIER)-${TEST_TYPE:=manual}
 
-if [[ -z $PARENT_FOLDER ]]
-then PARENT_FOLDER=$(gcloud projects describe ${PARENT_PROJECT} --format="value(parent.id)")
+if [[ -z $PARENT_FOLDER ]]; then
+    export PARENT_FOLDER=$(gcloud projects describe ${PARENT_PROJECT} --format="value(parent.id)")
 fi
 
 export BILLING_ACCOUNT=$(gcloud beta billing projects describe ${PARENT_PROJECT} --format="value(billingAccountName)" || sed -e 's/.*\///g')
 
+stepdo "🔨 create CI project"
 gcloud projects create ${CI_PROJECT} --folder ${PARENT_FOLDER}
-gcloud beta billing projects link ${CI_PROJECT} --billing-account=${BILLING_ACCOUNT}
+stepdone
+
+stepdo "check IAM policies"
+gcloud projects get-iam-policy ${CI_PROJECT}
 stepdone
 
 stepdo "assign IAM policies"
 quiet gcloud projects add-iam-policy-binding $CI_PROJECT \
     --member serviceAccount:${SA_EMAIL} \
-    --role roles/owner 
+    --role roles/iam.serviceAccountTokenCreator
+stepdone
+
+stepdo "setup billing"
+gcloud beta billing projects link ${CI_PROJECT} \
+    --billing-account=${BILLING_ACCOUNT} \
+    --log-http
 stepdone
 
 stepdo "enable services on ci project"
@@ -45,7 +57,6 @@ echo ""
 echo "✅ Project '${CI_PROJECT}' is now ready to use."
 echo ""
 
-
 if [[ "$TEST_TYPE" == "terraform" ]]; then
     stepdo "setting up Terraform state bucket"
     TF_STATE_BUCKET=${CI_PROJECT}-tfstate
@@ -53,14 +64,14 @@ if [[ "$TEST_TYPE" == "terraform" ]]; then
 
     echo $TF_STATE_BUCKET exists now. Adding to terraform manifests
 
-    cat > terraform/backend.tf <<_EOF
+    cat >terraform/backend.tf <<_EOF
 terraform { 
   backend gcs {
     bucket = "$TF_STATE_BUCKET"
   }
 }
 _EOF
-    cat terraform/backend.tf 
+    cat terraform/backend.tf
     stepdone
 fi
 
@@ -68,10 +79,13 @@ echo ""
 echo "🚀 Deploying with $CI_PROJECT from $CLOUDBUILD_CONFIG"
 echo ""
 
-gcloud builds submit --config $CLOUDBUILD_CONFIG --substitutions _CI_PROJECT=${CI_PROJECT} --timeout 1500
+gcloud builds submit \
+    --config $CLOUDBUILD_CONFIG \
+    --substitutions _CI_PROJECT=${CI_PROJECT} \
+    --timeout 1500
 statuscode=$?
 
-if [ $statuscode -ne 0 ]; then 
+if [ $statuscode -ne 0 ]; then
     echo "Cloud Build Failed. It may not be recoverable."
 else
     echo "✅ Success"
