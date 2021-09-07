cbd40fb0b99ee9bdb6d47a03301a5ece1d22bcd8
Katie McLaughlin <katie@glasnt.com>
2021-09-07 15:39:02 +1000

    init pytest attempt, minor fixes

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -2,3 +2,15 @@ resource "google_storage_bucket" "media" {
   name     = "${var.project}-media"
   location = "US"
 }
+
+data "google_iam_policy" "mediaaccess" {
+  binding {
+    role = "roles/storage.objectAdmin"
+    members = [local.unicodex_sa]
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket = google_storage_bucket.media.name
+  policy_data = data.google_iam_policy.mediaaccess.policy_data
+}
